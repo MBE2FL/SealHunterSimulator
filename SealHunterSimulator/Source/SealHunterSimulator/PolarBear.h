4c34a6901cbcd15944c2250ab3f2cd511b09d8ea
Max Kemp <max.kemp@ontariotechu.net>
2021-01-28 20:39:17 -0500

    Polar slowed down effect (untested).

--- SealHunterSimulator/Source/SealHunterSimulator/PolarBear.h
+++ SealHunterSimulator/Source/SealHunterSimulator/PolarBear.h
@@ -29,6 +29,17 @@ public:
 private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Polar Bear", meta = (AllowPrivateAccess = true))
 	float _health = 100.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Polar Bear", meta = (AllowPrivateAccess = true, ClampMin = "0.0"))
+	float _speedMultiplier = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Polar Bear", meta = (AllowPrivateAccess = true, ClampMin = "0.0"))
+	float _slowSpeedMultiplier = 1.0f;
+	float _currSpeedMultiplier = 1.0f;
+	bool _isSlow = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Polar Bear", meta = (AllowPrivateAccess = true, ClampMin = "0.0"))
+	float _slowEffectTime = 1.0f;
+	float _slowEffectTimer = 0.0f;
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Polar Bear", meta = (AllowPrivateAccess = true))
+	//FVector _movementVector = FVector::ZeroVector;
 
 
 	UFUNCTION()
@@ -38,4 +49,16 @@ private:
 		const class UDamageType* DamageType,
 		class AController* InstigatedBy,
 		AActor* DamageCauser);
+
+	UFUNCTION()
+	void moveForward(float value);
+
+	UFUNCTION()
+	void moveRight(float value);
+
+	UFUNCTION()
+	void turn(float value);
+
+	UFUNCTION()
+	void lookUp(float value);
 };
