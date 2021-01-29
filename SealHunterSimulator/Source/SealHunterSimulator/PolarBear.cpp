e39a1a34dd1a8635bf7db7a2ce08db05d976499b
NoahColangelo <noah.colangelo@uoit.net>
2021-01-28 20:45:20 -0500

    no message

--- SealHunterSimulator/Source/SealHunterSimulator/PolarBear.cpp
+++ SealHunterSimulator/Source/SealHunterSimulator/PolarBear.cpp
@@ -2,6 +2,7 @@
 
 
 #include "PolarBear.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 APolarBear::APolarBear()
@@ -17,6 +18,8 @@ void APolarBear::BeginPlay()
 	Super::BeginPlay();
 
 	OnTakeAnyDamage.AddDynamic(this, &APolarBear::onTakeAnyDamage);
+
+	_currSpeedMultiplier = _speedMultiplier;
 }
 
 // Called every frame
@@ -24,6 +27,17 @@ void APolarBear::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (_isSlow)
+	{
+		_slowEffectTimer += DeltaTime;
+
+		if (_slowEffectTimer >= _slowEffectTime)
+		{
+			_currSpeedMultiplier = _speedMultiplier;
+			_slowEffectTimer = 0.0f;
+			_isSlow = false;
+		}
+	}
 }
 
 // Called to bind functionality to input
@@ -31,6 +45,10 @@ void APolarBear::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &APolarBear::moveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &APolarBear::moveRight);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &APolarBear::turn);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APolarBear::lookUp);
 }
 
 void APolarBear::onTakeAnyDamage(
@@ -48,5 +66,39 @@ void APolarBear::onTakeAnyDamage(
 		UE_LOG(LogTemp, Warning, TEXT("Polar Bear Died!"));
 		Destroy();
 	}
+
+
+	_isSlow = true;
+	_currSpeedMultiplier = _slowSpeedMultiplier;
+}
+
+void APolarBear::moveForward(float value)
+{
+	FRotator rotator = GetControlRotation();
+	rotator.Roll = 0.0f;
+	AddMovementInput(UKismetMathLibrary::GetForwardVector(rotator), value * _currSpeedMultiplier);
+
+	//AddMovementInput(GetActorForwardVector(), value * _speedMultiplier);
+	//_movementVector += GetActorForwardVector() * value;
+}
+
+void APolarBear::moveRight(float value)
+{
+	FRotator rotator = GetControlRotation();
+	rotator.Roll = 0.0f;
+	AddMovementInput(UKismetMathLibrary::GetRightVector(rotator), value * _currSpeedMultiplier);
+
+	//AddMovementInput(GetActorRightVector(), value * _speedMultiplier);
+	//_movementVector += GetActorRightVector() * value;
+}
+
+void APolarBear::turn(float value)
+{
+	AddControllerYawInput(value);
+}
+
+void APolarBear::lookUp(float value)
+{
+	AddControllerPitchInput(value);
 }
 
